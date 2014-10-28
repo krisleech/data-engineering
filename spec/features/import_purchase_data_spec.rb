5e5f02ff48701115f360ec4b2e7c40864fcc9f96
Kris Leech <kris.leech@gmail.com>
2014-10-28 17:07:08 +0000

    add missing feature spec and reword a little

--- spec/features/import_purchase_data_spec.rb
+++ spec/features/import_purchase_data_spec.rb
@@ -1,21 +1,36 @@
 require 'support/feature_helper'
 
-feature 'importing a CSV file' do
-  let(:csv_fixture_path) { File.expand_path('spec/fixtures/valid_sample.tsv')  }
+feature 'importing data' do
 
-  scenario 'uploading a valid CSV file' do
-    visit root_url
-    attach_file('file', csv_fixture_path)
-    click_button 'Submit'
+  context 'with a valid TSV file' do
+    let(:csv_fixture_path) { File.expand_path('spec/fixtures/valid_sample.tsv')  }
 
-    expect(page).to have_content('Import successful')
+    scenario 'successful import' do
+      visit root_url
+      attach_file('file', csv_fixture_path)
+      click_button 'Submit'
+
+      expect(page).to have_content('Import successful')
+    end
+
+    scenario 'displays gross revenue' do
+      visit root_url
+      attach_file('file', csv_fixture_path)
+      click_button 'Submit'
+
+      expect(page).to have_content('Gross Revenue 95.0')
+    end
   end
 
-  scenario 'displays gross revenue' do
-    visit root_url
-    attach_file('file', csv_fixture_path)
-    click_button 'Submit'
+  context 'with an invalid tsv file' do
+    let(:csv_fixture_path) { File.expand_path('spec/fixtures/invalid_sample.tsv')  }
 
-    expect(page).to have_content('Gross Revenue 95.0')
+    scenario 'unsuccessful import' do
+      visit root_url
+      attach_file('file', csv_fixture_path)
+      click_button 'Submit'
+
+      expect(page).to have_content('Import failed')
+    end
   end
 end
